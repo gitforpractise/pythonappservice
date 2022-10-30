b8be995decd9d961d0ed8f47b22d2817ef90fe2b
gitforpractise <95076444+gitforpractise@users.noreply.github.com>
2022-10-30 20:18:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from App Services..."
+    return "Hello from App Service from azure platform"
 
 if __name__ == '__main__':
    app.run()
